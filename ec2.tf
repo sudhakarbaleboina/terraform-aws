00e90147dcd72eeef8b9e138b6f9a89e65e493c6
Sudhakar Baleboina <86168545+sudhakarbaleboina@users.noreply.github.com>
2023-04-15 21:42:27 +0530

    Add files via upload

---
+++ ec2.tf
@@ -0,0 +1,24 @@
+
+resource "aws_instance" "frontend" {
+  ami           = "ami-038face4e75b6a399"
+  instance_type = "t2.micro"
+  key_name      = "sudhakar"
+  root_block_device {
+    delete_on_termination = true
+    volume_size = "8"
+    volume_type = "gp2"
+  }
+
+  tags = {
+    Name = "frontend-cicd-terraform"
+  }
+}
+
+resource "aws_s3_bucket" "backend" {
+  bucket = "sudhakar-venu-sudhakar"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
---
+++ provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+#backend
+terraform {
+  backend "s3" {
+    bucket = "sudhakar-venu-sudhakar"
+    key    = "practice/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+}
